866eb1e4afb59c2b611f0a00da872e260b39ffa8
Halyna Marchenko <marchenkohalyna888@gmail.com>
2025-03-29 21:56:19 +0100

    set up home task

---
+++ bot_project/assistant_bot/data.py
@@ -0,0 +1,96 @@
+from collections import UserDict
+
+contacts = {}
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+    
+    def __str__(self):
+        return str(self.value)
+    
+
+class Name(Field):
+    pass
+
+class Phone(Field):
+    def __init__(self, value):
+        if len(value) != 10 or not value.isdigit():
+            raise ValueError("Phone number must be 10 digits.")
+        super().__init__(value)
+
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+
+    def add_phone(self, phone):
+        self.phones.append(Phone(phone))
+
+    def remove_phone(self, phone):
+        for p in self.phones:
+            if p.value == phone:
+                self.phones.remove(p)
+                return
+            raise ValueError("Phone number not found.")
+    
+    def edit_phone(self, old_phone, new_phone):
+        for p in self.phones:
+            if p.value == old_phone:
+                self.phones.remove(p)
+                self.add_phone(new_phone)
+                return
+        raise ValueError("Old phone number not found.")
+        
+    def find_phone(self, phone):
+        for p in self.phones:
+            if p.value == phone:
+                return p
+        return None
+        
+    def __str__(self):
+        return f"Contact name: {self.name.value}, phones: {", ".join(p.value for p in self.phones)}"
+    
+
+class AddressBook(UserDict):
+    def add_record(self, record):
+        self.data[record.name.value] = record
+    
+    def find(self, name):
+        return self.data.get(name)
+    
+    def delete(self, name):
+        if name in self.data:
+            del self.data[name]
+        else:
+            raise KeyError(f"Record with name {name} not found.")
+        
+    def __str__(self):
+        if not self.data:
+            return "The contact list is empty."
+        return "\n".join(str(record) for record in self.data.values())
+    
+    
+if __name__ == "__main__":
+    book = AddressBook()
+
+    john_record = Record("John")
+    john_record.add_phone("1234567890")
+    john_record.add_phone("5555555555")
+    book.add_record(john_record)
+
+    jane_record = Record("Jane")
+    jane_record.add_phone("9876543210")
+    book.add_record(jane_record)
+
+    print(book)
+
+    john = book.find("John")
+    john.edit_phone("1234567890", "1112223333")
+    print(john)
+
+    found_phone = john.find_phone("5555555555")
+    print(f"{john.name}: {found_phone.value}")
+
+    book.delete("Jane")
+    print(book)
